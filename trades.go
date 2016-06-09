5ea42e5c9fb572372b3ac905c7f7f7bcac5332af
cluda <asgeir@cluda.no>
2016-06-09 15:10:12 +0200

    Trade price and amount are now big.Float (instead of int)

--- trades.go
+++ trades.go
@@ -1,45 +1,46 @@
 package bitfinex
 
 import (
-    "net/url"
-    "strconv"
-    "strings"
+	"math/big"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 type TradesService struct {
-    client *Client
+	client *Client
 }
 
 type Trade struct {
-    Price     string
-    Amount    string
-    Exchange  string
-    Type      string
-    Timestamp int64
-    TradeId   int64 `json:"tid,int"`
+	Price     big.Float
+	Amount    big.Float
+	Exchange  string
+	Type      string
+	Timestamp int64
+	TradeId   int64 `json:"tid,int"`
 }
 
 func (s *TradesService) All(pair string, timestamp int64, limitTrades int) ([]Trade, error) {
-    pair = strings.ToUpper(pair)
+	pair = strings.ToUpper(pair)
 
-    params := url.Values{}
-    if timestamp != 0 {
-        params.Add("timestamp", strconv.FormatInt(timestamp, 10))
-    }
-    if limitTrades != 0 {
-        params.Add("limit_trades", strconv.Itoa(limitTrades))
-    }
-    req, err := s.client.newRequest("GET", "trades/"+pair, params)
-    if err != nil {
-        return nil, err
-    }
+	params := url.Values{}
+	if timestamp != 0 {
+		params.Add("timestamp", strconv.FormatInt(timestamp, 10))
+	}
+	if limitTrades != 0 {
+		params.Add("limit_trades", strconv.Itoa(limitTrades))
+	}
+	req, err := s.client.newRequest("GET", "trades/"+pair, params)
+	if err != nil {
+		return nil, err
+	}
 
-    var v []Trade
+	var v []Trade
 
-    _, err = s.client.do(req, &v)
-    if err != nil {
-        return nil, err
-    }
+	_, err = s.client.do(req, &v)
+	if err != nil {
+		return nil, err
+	}
 
-    return v, nil
+	return v, nil
 }
